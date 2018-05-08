cf7c9729988d61fce5590bc956d728f8d7a6a57b
leonardo2204 <ferrari2204_5@hotmail.com>
2018-05-07 22:51:57 -0300

    load more when reaching end

--- Components/ConferenceList/ConferenceList.js
+++ Components/ConferenceList/ConferenceList.js
@@ -6,39 +6,49 @@ import {
   Button,
   Platform,
   TouchableOpacity,
-  ActivityIndicator
+  ActivityIndicator,
 } from 'react-native';
 
 import { Divider, Header, Icon } from "react-native-elements";
 import { connectInfiniteHits, connectStateResults } from 'react-instantsearch/connectors';
 import s from './ConferenceListStyle'
 import ConferenceItem from '../ConferenceItem/ConferenceItem'
+import ProgressBar from 'react-native-progress/Bar'
 
 export default connectInfiniteHits(({ hits, hasMore, refine }) => {
-  const renderLoadMoreFooter = () => {
-    return (hasMore &&
-      <View style={s.loadMoreButtonContainer}>
-        <Divider style={s.loadMoreButtonDivider} />
-        <MoreResultsFooter />
-      </View>
-    )
-  }
 
-  const MoreResultsFooter = connectStateResults(
+  const LoadMoreFooter = connectStateResults(
     ({ searching }) =>
-      searching ? <ActivityIndicator size={'large'} color={'black'} /> : <Button title={'Load more confs...'} onPress={() => refine()} />
+      !searching ? <View style={s.loadMoreButtonContainer}>
+        <Divider style={s.loadMoreButtonDivider} />
+        <Button title={'Load more confs...'} onPress={() => refine()} />
+      </View> : null
   );
+
+  const LoadingIndicator = connectStateResults(
+    ({ searching }) =>
+      searching ? <ProgressBar indeterminate width={null} borderWidth={0} color={'black'} height={3} /> : null
+  );
+
+  const onEndReached = () => {
+    if (hasMore) {
+      refine();
+    }
+  };
 
   return (
     <FlatList
       data={hits}
       keyExtractor={(item, index) => item.objectID}
-      ListFooterComponent={renderLoadMoreFooter}
-      ListHeaderComponent={<Header
-        outerContainerStyles={{ height: Platform.OS === 'ios' ? 70 : 70 - 24 }}
-        centerComponent={<Text style={{ color: 'white' }}>Confs.tech</Text>}
-      //rightComponent={<TouchableOpacity><Icon color={'white'} name={'filter-list'} /></TouchableOpacity>} 
-      />}
+      onEndReached={this.onEndReached}
+      ListHeaderComponent={
+        <View>
+          <Header
+            outerContainerStyles={{ height: Platform.OS === 'ios' ? 70 : 70 - 24 }}
+            centerComponent={<Text style={{ color: 'white' }}>Confs.tech</Text>}
+          //rightComponent={<TouchableOpacity><Icon color={'white'} name={'filter-list'} /></TouchableOpacity>} 
+          /><LoadingIndicator />
+        </View>}
       stickyHeaderIndices={[0]}
       renderItem={({ item }) =>
         <ConferenceItem {...item} />
