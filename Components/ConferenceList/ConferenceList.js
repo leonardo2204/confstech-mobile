1e46121f32982a15d536329cac72d02d6da7c407
leonardo2204 <ferrari2204_5@hotmail.com>
2018-05-10 01:13:20 -0300

    style load more btn

--- Components/ConferenceList/ConferenceList.js
+++ Components/ConferenceList/ConferenceList.js
@@ -3,13 +3,12 @@ import {
   Text,
   View,
   FlatList,
-  Button,
   Platform,
   TouchableOpacity,
   ActivityIndicator,
 } from 'react-native';
 
-import { Divider, Header, Icon } from "react-native-elements";
+import { Divider, Header, Icon, Button } from "react-native-elements";
 import { connectInfiniteHits, connectStateResults } from 'react-instantsearch/connectors';
 import s from './ConferenceListStyle'
 import ConferenceItem from '../ConferenceItem/ConferenceItem'
@@ -21,7 +20,7 @@ export default connectInfiniteHits(({ hits, hasMore, refine }) => {
     ({ searching }) =>
       !searching ? <View style={s.loadMoreButtonContainer}>
         <Divider style={s.loadMoreButtonDivider} />
-        <Button title={'Load more confs...'} onPress={() => refine()} />
+        <Button transparent color={'#53acfe'} title={'Load more confs...'} onPress={() => refine()} />
       </View> : null
   );
 
